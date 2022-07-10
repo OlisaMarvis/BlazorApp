3f1315b2dd8e4ff8c493336b0f12c6c82c9c92c0
OlisaMarvis <Olisamarvis@gmail.com>
2022-07-10 04:39:37 +0100

    Add project files.

---
+++ EmployeeManagement.models/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+
+    }
+}
